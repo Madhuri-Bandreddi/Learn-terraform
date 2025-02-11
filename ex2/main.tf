70bff99f2cedaa0b05ff8fb441bfb866f0ace918
mammu <bandreddymammu599@gmail.com>
2025-02-11 17:26:43 +0530

    terraform

--- ex2/main.tf
+++ ex2/main.tf
@@ -6,3 +6,4 @@ module "ec2" {
   source = "./ec2"
   sg_id  = module.sg.sg_id
 }
+
--- import/main.tf
+++ import/main.tf
@@ -9,3 +9,4 @@ resource "aws_instance" "workstation" {
 output "private_ip" {
   value = aws_instance.workstation.private_ip
 }
+
