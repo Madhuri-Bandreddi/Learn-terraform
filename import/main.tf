a6a75568490e4f7893d44c00be3b94b575d60415
mammu <bandreddymammu599@gmail.com>
2025-02-11 17:40:58 +0530

    terraform

--- import/main.tf
+++ import/main.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "workstation" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  #ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami            = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
   tags   = {
     "Name" = "workstation"
